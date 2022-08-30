69188d2ca5951364205ade651de3a080acab85a9
TMartin88 <tiernanmartin1@gmail.com>
2022-08-30 08:37:19 +0000

    Change json data and json loading code

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,7 +65,31 @@ document.getElementById("fare_required").style.fontWeight = "900";
 
 }
 
-fetch('assets/js/data.json')
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-  
+async function getUsers() {
+  let url = 'assets/js/data.json';
+  try {
+      let res = await fetch(url);
+      return await res.json();
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+async function renderUsers() {
+  let users = await getUsers();
+  let html = '';
+  users.forEach(user => {
+      let htmlSegment = `<div class="user">
+                          <img src="${user.profileURL}" >
+                          <h2>${user.firstName} ${user.lastName}</h2>
+                          <div class="email"><a href="email:${user.email}">${user.email}</a></div>
+                      </div>`;
+
+      html += htmlSegment;
+  });
+
+  let container = document.querySelector('.container');
+  container.innerHTML = html;
+}
+
+renderUsers();
